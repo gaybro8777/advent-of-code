4af66c2b6bcfb3d55c1ebe43ce7a6b3994812d05
reuk <reuk@users.noreply.github.com>
2019-12-04 14:24:11 +0000

    Fix haskell syntax

--- day4/src/Main.hs
+++ day4/src/Main.hs
@@ -5,10 +5,10 @@ import Data.List.Ordered
 count = (length .) . filter
 
 isValid extraCheck pwd = let chars = show pwd in
-  let ordered = isSorted chars in
-  let duplicateFinder elem = extraCheck (count (== elem) chars) 2 in
-  let duplicatesFound = any duplicateFinder chars in
-  ordered && duplicatesFound
+  let ordered = isSorted chars
+      duplicateFinder elem = extraCheck (count (== elem) chars) 2
+      duplicatesFound = any duplicateFinder chars
+  in ordered && duplicatesFound
 
 countValid = flip count [153517..630395] . isValid
 
