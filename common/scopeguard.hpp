75e0457daf92044b296ab668d6bd07fc690eb712
reuk <reuk@users.noreply.github.com>
2019-12-27 18:56:42 +0000

    fix scopeguard formatting

--- common/scopeguard.hpp
+++ common/scopeguard.hpp
@@ -3,11 +3,10 @@
 namespace aoc {
 
 template <typename Ts>
-struct [[nodiscard]] ScopeGuard final
-    : Ts{~ScopeGuard() noexcept {Ts::operator()();
-}
-}
-;
+struct [[nodiscard]] ScopeGuard final : Ts {
+    ~ScopeGuard() noexcept {Ts::operator()(); }
+};
 
 template <typename Ts> ScopeGuard(Ts)->ScopeGuard<Ts>;
+
 }
